0f378d12259c332d398a2fe0e4f5befa2cf20765
Shea McKinney <sherakama@gmail.com>
2017-02-20 16:03:13 -0800

    Examples

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,13 +19,37 @@ module.exports = function(grunt) {
       default: {
         src: 'core',
       }
-    }
+    },
+    sass: {
+      // This will compile all of our sass files
+      // Additional configuration options can be found at https://github.com/sindresorhus/grunt-sass
+      options: {
+        includePaths: [
+          "node_modules/bourbon/core",
+          "node_modules/bourbon-neat/core",
+          "node_modules/font-awesome/scss",
+          "node_modules/normalize.css",
+          "node_modules",
+          "core"
+        ],
+        sourceMap: true,
+        // This controls the compiled css and can be changed to nested, compact or compressed.
+        outputStyle: 'expanded',
+        precision: 5
+      },
+      dist: {
+        files: {
+          'examples/css/index.css': 'examples/scss/index.scss',
+        }
+      }
+    },
   });
 
   // This is where we tell Grunt we plan to use this plug-in.
-  grunt.loadNpmTasks('grunt-sass-lint');
   grunt.loadNpmTasks('grunt-available-tasks');
   grunt.loadNpmTasks('grunt-sassdoc');
+  grunt.loadNpmTasks('grunt-sass-lint');
+  grunt.loadNpmTasks('grunt-sass');
 
   // This is where we tell Grunt what to do when we type "grunt" into the terminal.
   grunt.registerTask('default', ['availabletasks']);
