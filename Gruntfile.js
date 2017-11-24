fde3756e1843d5fddc1e23a8881ea3fb6e7d36c2
Shea McKinney <sherakama@gmail.com>
2017-11-24 09:32:58 -0800

    Rolld back to older versions of bourbon/neat.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,8 @@ module.exports = function(grunt) {
     sass: {
       options: {
         includePaths: [
-          "node_modules/bourbon/core",
-          "node_modules/bourbon-neat/core",
+          "node_modules/bourbon/app/assets/stylesheets",
+          "node_modules/bourbon-neat/app/assets/stylesheets",
           "node_modules/font-awesome/scss",
           "node_modules/normalize.css",
           "node_modules/neat-omega/core",
