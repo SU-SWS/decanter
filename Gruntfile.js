0e172db2e98e9921cd532e15595975c71ce49992
Richard Gilbert <gilbert67@gmail.com>
2018-07-05 23:06:04 -0700

    Decanter 159: Updating modules and grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,10 @@ module.exports = function(grunt) {
       options: {
         includePaths: [
           "node_modules/bourbon/core",
-          "node_modules/bourbon-neat/app/assets/stylesheets",
-          "node_modules/normalize.css",
+          "node_modules/bourbon-neat/core",
+          "node_modules/breakpoint-sass/stylesheets",
           "node_modules/neat-omega/core",
+          "node_modules/normalize.css",
           "node_modules",
           "scss"
         ],
