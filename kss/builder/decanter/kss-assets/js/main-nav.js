2387aa11f514aeef21a154e21389ea400d62460c
Shea McKinney <sherakama@gmail.com>
2018-11-30 00:58:48 -0800

    More KSS theme tweaks.

--- kss/builder/decanter/kss-assets/js/main-nav.js
+++ kss/builder/decanter/kss-assets/js/main-nav.js
@@ -25,5 +25,34 @@
     }
   });
 
+  // Indent third level items a bit.
+  var last_child = 1;
+  $(".kss-nav__ref-child, .kss-nav__ref").each(function(i, v) {
+    var count = $(v).text().match(/\./g);
+    var depth = 0;
+
+    if (count !== null) {
+      depth = count.length;
+    }
+
+    if (depth >= 1) {
+      $(v).parent().parent().addClass('child-nav-level-' + depth);
+    }
+
+    if (depth == 4 && last_child == 3) {
+      $(v).parent().parent().prev().find("a").addClass("separator-child");
+    }
+
+    if (depth == 3 && last_child == 2) {
+      $(v).parent().parent().prev().find("a").addClass("separator-child");
+    }
+
+    if (depth == 2 && last_child == 1) {
+      $(v).parent().parent().prev().find("a").addClass("separator-child");
+    }
+
+    last_child = depth;
+  });
+
   $(window).scroll();
 })(jQuery);
