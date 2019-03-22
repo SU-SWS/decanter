9733779cd176c45355be3382108fa5bb3f02eac9
Sherakama <sherakama@gmail.com>
2019-03-22 14:57:52 -0700

    90 packs (#353)
    * src & dist directory
    * Working watch on all JS, SASS, Twig, and JSON
    * Adds working map files
    * Restores some Grunt tasks and replaces what now has npm scripts with a grunt run npm script instead.
    * Moved assets to kss-assets where they should have been in the first place
    * Changed up the npm scripts so they don't call everything all the time. Added a new dev function to do that.
    * Compiles the KSS theme as well.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
+/**
+ * Webpack Configuration File
+ * @type {[type]}
+ */
+
+ // Requires / Dependencies.
 const path = require('path');
-
-const assetDir  = './core/';
-const outputDir = 'core/dist';
-const styleGuide = path.resolve( __dirname, './styleguide/');
-
-const npmPackage = './node_modules/';
-const devMode = process.env.NODE_ENV !== 'production';
-
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -15,29 +13,161 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const WebpackAssetsManifest = require("webpack-assets-manifest");
 const CopyPlugin = require('webpack-copy-on-build-plugin');
+const FilewatcherPlugin = require("filewatcher-webpack-plugin");
 
-// for MiniCssExtractPlugin
+// Paths.
+const assetDir  = './src/';
+const kssDir  = './kss/builder/decanter/';
+const outputDir = 'dist';
+const styleGuide = path.resolve( __dirname, './styleguide/');
+const npmPackage = 'node_modules/';
+
+// Other variables.
+const devMode = process.env.NODE_ENV !== 'production';
+
+// For MiniCssExtractPlugin.
+// Loops through the module variable that is nested looking for a name.
 function recursiveIssuer(module) {
   if (module.issuer) {
     return recursiveIssuer(module.issuer);
-  } else if (module.name) {
+  }
+  else if (module.name) {
     return module.name;
-  } else {
+  }
+  else {
     return false;
   }
 }
 
-module.exports = {
+// Common config between Decanter and KSS builds.
+var config = {
+  // Allows for map files.
+  devtool: 'source-map',
+  // Define modules.
+  module: {
+    rules: [
+      // Apply babel ES6 compilation to JavaScript files.
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [ '@babel/preset-env' ]
+          }
+        }
+      },
+      // Apply Plugins to SCSS/SASS files.
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          // Extract loader.
+          MiniCssExtractPlugin.loader,
+          // CSS Loader. Generate sourceMaps.
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              url: true
+            }
+          },
+          // Post CSS. Run autoprefixer plugin.
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: () => [
+                autoprefixer( {
+                  browsers: [ 'last 2 versions', 'ie 11' ]
+                } )
+              ]
+            }
+          },
+          // SASS Loader. Add compile paths to include bourbon.
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                path.resolve( __dirname, npmPackage + "bourbon/core" ),
+                path.resolve( __dirname, "src/scss" )
+              ],
+              sourceMap: true,
+              lineNumbers: true,
+              outputStyle: 'nested',
+              precision: 10
+            }
+          }
+        ]
+      },
+      // Apply plugins to image assets.
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        use: [
+          // A loader for webpack which transforms files into base64 URIs.
+          // https://github.com/webpack-contrib/url-loader
+          {
+            loader: 'url-loader',
+            options: {
+              // Maximum size of a file in bytes. 8.192 Kilobtyes.
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  // Define and configure webpack plugins.
+  plugins: [
+    // A webpack plugin to remove/clean your build folder(s).
+    // https://www.npmjs.com/package/clean-webpack-plugin
+    new CleanWebpackPlugin( {
+      verbose: true
+    } ),
+    // This plugin extracts CSS into separate files. It creates a CSS file per
+    // JS file which contains CSS. It supports On-Demand-Loading of CSS and
+    // SourceMaps.
+    // https://github.com/webpack-contrib/mini-css-extract-plugin
+    new MiniCssExtractPlugin( {
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "../css/[name].css",
+      chunkFilename: "../css/[id].css"
+    } ),
+    // This Webpack plugin will generate a JSON file that matches the original
+    // filename with the hashed version.
+    // https://github.com/webdeveric/webpack-assets-manifest
+    new WebpackAssetsManifest( {
+      output: 'assets.json'
+    } ),
+    // Add a plugin to watch other files other than that required by webpack.
+    // https://www.npmjs.com/package/filewatcher-webpack-plugin
+    new FilewatcherPlugin( {
+      watchFileRegex: [
+        'src/**/*.twig',
+        'src/**/*.json'
+      ]
+    } ),
+  ]
+};
+
+// Decanter core configuration.
+let coreConfig = Object.assign({}, config, {
+  name: "decanter",
+  // Define the entry points for which webpack builds a dependency graph.
   entry: {
     "decanter": assetDir + "js/decanter.js",
     "decanter-grid": assetDir + "js/decanter-grid.js",
-    "decanter-no-markup": assetDir + "js/decanter-no-markup.js"
+    "decanter-no-markup": assetDir + "js/decanter-no-markup.js",
+    'kss': kssDir + "scss/kss.js"
   },
+  // Where should I output the assets.
   output: {
     filename: devMode ? "[name].js" : "[name].[hash].js",
     path: path.resolve( __dirname, outputDir + '/js' )
   },
+  // Optimizations that are triggered by production mode.
   optimization: {
+    // Uglify the Javascript & and CSS.
     minimizer: [
       new UglifyJsPlugin( {
         cache: true,
@@ -46,6 +176,8 @@ module.exports = {
       } ),
       new OptimizeCSSAssetsPlugin( {} )
     ],
+    // Splitchunks plugin configuration.
+    // https://webpack.js.org/plugins/split-chunks-plugin/.
     splitChunks: {
       cacheGroups: {
         'decanter': {
@@ -59,94 +191,55 @@ module.exports = {
           test: ( module, chunks, entry = 'decanter-no-markup' ) => module.constructor.name === 'CssModule' && recursiveIssuer( module ) === entry,
           chunks: 'all',
           enforce: true
+        },
+        'kss': {
+          name: 'kss',
+          test: ( module, chunks, entry = 'kss' ) => module.constructor.name === 'CssModule' && recursiveIssuer( module ) === entry,
+          chunks: 'all',
+          enforce: true
         }
       }
     }
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [ '@babel/preset-env' ]
-          }
-        }
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {}
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              url: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              plugins: () => [
-                autoprefixer( {
-                  browsers: [ 'last 2 versions', '> 1%' ]
-                } )
-              ]
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [
-                path.resolve( __dirname, npmPackage, 'bourbon/core' )
-              ],
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192
-            }
-          }
-        ]
-      }
-    ]
+});
+
+// Decanter core configuration.
+let kssConfig = Object.assign({}, config, {
+  name: "kss",
+  // Define the entry points for which webpack builds a dependency graph.
+  entry: {
+    'kss': kssDir + "scss/kss.js"
   },
-  plugins: [
-    new CleanWebpackPlugin( {
-      verbose: true
-    } ),
-    new MiniCssExtractPlugin( {
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "../css/[name].css",
-      chunkFilename: "../css/[id].css",
-      sourceMap: true
-    } ),
-    new WebpackAssetsManifest( {
-      output: 'assets.json'
-    } ),
-    new CopyPlugin( [
-      {
-        from: outputDir + "/css/decanter.css",
-        to: styleGuide + '/css/decanter.css',
-      },
-      {
-        from: outputDir + "/js/decanter.js",
-        to: styleGuide + "/js/decanter.js",
-      },
-    ] ),
-  ]
-}
+  // Where should I output the assets.
+  output: {
+    filename: devMode ? "[name].js" : "[name].[hash].js",
+    path: path.resolve( __dirname, kssDir + 'kss-assets/dist' )
+  },
+  // Optimizations that are triggered by production mode.
+  optimization: {
+    // Uglify the Javascript & and CSS.
+    minimizer: [
+      new UglifyJsPlugin( {
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      } ),
+      new OptimizeCSSAssetsPlugin( {} )
+    ],
+    // Splitchunks plugin configuration.
+    // https://webpack.js.org/plugins/split-chunks-plugin/.
+    splitChunks: {
+      cacheGroups: {
+        'kss': {
+          name: 'kss',
+          test: ( module, chunks, entry = 'kss' ) => module.constructor.name === 'CssModule' && recursiveIssuer( module ) === entry,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+});
+
+// Module Exports.
+module.exports = [ coreConfig, kssConfig ];
