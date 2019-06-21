76871bdbf02624da6a28a7e4c58b627970a99b94
Shea McKinney <sherakama@gmail.com>
2019-06-21 15:10:50 -0700

    Restored more keyboard controls.

--- core/src/js/components/main-nav/NavItem.js
+++ core/src/js/components/main-nav/NavItem.js
@@ -1,4 +1,4 @@
-import {isHome, isEnd, isTab, isSpace, isEnter, isLeftArrow, isRightArrow, isUpArrow, isDownArrow} from "../../utilities/keyboard";
+import {normalizeKey} from "../../utilities/keyboard";
 import Nav from './Nav';
 import {createEvent} from '../../utilities/events';
 
@@ -76,16 +76,145 @@ export default class NavItem {
    */
   onKeydown(event, target) {
     let theKey = event.key || event.keyCode;
+    let normalized = normalizeKey(theKey);
+
+    // We don't know about that key.
+    if (!normalized) {
+      return;
+    }
+
+    // Prepare a dynamic handler.
+    let handler = 'onKeydown'
+      + normalized.charAt(0).toUpperCase()
+      + normalized.slice(1);
+    if (typeof this[handler] === 'function') {
+      return this[handler](event, target);
+    }
   }
 
   /**
-   * [onClick description]
-   * @param  {[type]} event  [description]
-   * @param  {[type]} target [description]
-   * @return {[type]}        [description]
-   */
-  onClick(event, target) {
-    console.log(target);
+   * Handler for keypress of
+   *
+   **/
+  onKeydownHome(event, target) {
+    event.preventDefault();
+    // Focus on the first item in the same level of this list.
+    this.link.parentNode.parentNode.firstElementChild.firstChild.focus();
+  }
+
+  /**
+   * Handler for keypress of
+   *
+   **/
+  onKeydownEnd(event, target) {
+    event.preventDefault();
+    // Focus on the last item in the same level of this list.
+    this.link.parentNode.parentNode.lastElementChild.firstChild.focus();
+  }
+
+  /**
+   * Handler for keypress of
+   *
+   **/
+  onKeydownTab(event, target) {
+    // Nada. Let the browser default run.
+  }
+
+  /**
+   * Handler for keypress of
+   *
+   **/
+  onKeydownEscape(event, target) {
+    event.preventDefault();
+    // Close everything.
+    this.nav.closeAllSubNavs();
+  }
+
+  /**
+   * Handler for keypress of
+   **/
+  onKeydownSpace(event, target) {
+    // Nada, let the browser default run.
+  }
+
+  /**
+   * Handler for keypress of
+   *
+   **/
+  onKeydownEnter(event, target) {
+    // Nada, let the browser default run.
+  }
+
+  /**
+   * Handler for keypress of
+   *
+   **/
+  onKeydownArrowLeft(event, target) {
+    event.preventDefault();
+    // Go to the previous item.
+    let node = this.link.parentNode.previousElementSibling;
+    if (node !== null) {
+      node.firstChild.focus();
+    }
+    else {
+      this.onKeydownEnd(event, target);
+    }
+  }
+
+  /**
+   * Handler for keypress of
+   *
+   **/
+  onKeydownArrowRight(event, target) {
+    event.preventDefault();
+    // Go to the next item.
+    let node = this.link.parentNode.nextElementSibling;
+    if (node !== null) {
+      node.firstChild.focus();
+    }
+    else {
+      this.onKeydownHome(event, target);
+    }
+  }
+
+  /**
+   * Handler for keypress of
+   *
+   **/
+  onKeydownArrowUp(event, target) {
+    event.preventDefault();
+    // Go to the previous item.
+    let node = this.link.parentNode.previousElementSibling;
+    if (node !== null) {
+      node.firstChild.focus();
+    }
+    else {
+      // If this is a nav item in a subnav. Go up a level and close the subnav.
+      if (this.item.parentNode.parentNode.classList.contains(this.options.itemExpandedClass)){
+        this.nav.closeAllSubNavs();
+        this.item.parentNode.parentNode.querySelector("a").focus();
+      }
+      // Circle back to the last item.
+      else {
+        this.onKeydownEnd(event, target);
+      }
+    }
+  }
+
+  /**
+   * Handler for keypress of
+   *
+   **/
+  onKeydownArrowDown(event, target) {
+    event.preventDefault();
+    // Go to the next item.
+    let node = this.link.parentNode.nextElementSibling;
+    if (node !== null) {
+      node.firstChild.focus();
+    }
+    else {
+      this.onKeydownHome(event, target);
+    }
   }
 
 }
--- core/src/js/utilities/keyboard.js
+++ core/src/js/utilities/keyboard.js
@@ -12,3 +12,43 @@ export const isLeftArrow = theKey => (theKey === 'ArrowLeft' || theKey === 'Left
 export const isRightArrow = theKey => (theKey === 'ArrowRight' || theKey === 'Right' || theKey === 39);
 export const isUpArrow = theKey => (theKey === 'ArrowUp' || theKey === 'Up' || theKey === 38);
 export const isDownArrow = theKey => (theKey === 'ArrowDown' || theKey === 'Down' || theKey === 40);
+
+/**
+ * [normalizeKey description]
+ * @param  {[type]} theKey [description]
+ * @return {[type]}        [description]
+ */
+export const normalizeKey = (theKey) => {
+  if(isHome(theKey)) {
+    return "home";
+  }
+  if(isEnd(theKey)) {
+    return "end";
+  }
+  if(isTab(theKey)) {
+    return "tab";
+  }
+  if(isEsc(theKey)) {
+    return "escape";
+  }
+  if(isSpace(theKey)) {
+    return "space";
+  }
+  if(isEnter(theKey)) {
+    return "enter";
+  }
+  if(isLeftArrow(theKey)) {
+    return "arrowLeft";
+  }
+  if(isRightArrow(theKey)) {
+    return "arrowRight";
+  }
+  if(isUpArrow(theKey)) {
+    return "arrowUp";
+  }
+  if(isDownArrow(theKey)) {
+    return "arrowDown";
+  }
+
+  return false;
+}
