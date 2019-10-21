9199815da8f76ac95ee9ff2d95237e094d4a99e2
Mike Decker <pookmish@gmail.com>
2019-10-21 14:15:39 -0700

    moved commands around

--- .tugboat/init.sh
+++ .tugboat/init.sh
@@ -9,10 +9,4 @@ source ~/.bashrc
 httpd
 
 nvm install 10.15.1
-nvm use
-npm install
-npm run build
-npm run styleguide
-rm -R node_modules
-rm -rf /var/www/localhost/htdocs
-ln -snf "${TUGBOAT_ROOT}/styleguide" /var/www/localhost/htdocs
+
