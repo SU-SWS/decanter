d331e75aa017c614e69fbd61afb5172716db1f08
Mike Decker <pookmish@gmail.com>
2019-10-21 14:11:33 -0700

    install apache

--- .tugboat/init.sh
+++ .tugboat/init.sh
@@ -2,10 +2,11 @@
 
 touch ~/.bashrc
 
-apk add -U curl bash ca-certificates openssl ncurses coreutils python2 make gcc g++ libgcc linux-headers grep util-linux binutils findutils nodejs npm
+apk add -U curl bash ca-certificates openssl ncurses coreutils python2 make gcc g++ libgcc linux-headers grep util-linux binutils findutils nodejs npm apache2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 
 source ~/.bashrc
+httpd
 
 nvm use
 npm install
