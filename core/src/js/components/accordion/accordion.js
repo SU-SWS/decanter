ad1e04f26de45e9a20b85ce1994c42bc027d9b97
Yvonne Tang <ytang2@stanford.edu>
2020-03-12 15:01:21 -0700

    Add expand/collapse all buttons and functionality

--- core/src/js/components/accordion/accordion.js
+++ core/src/js/components/accordion/accordion.js
@@ -1,5 +1,7 @@
 const accordions = document.querySelectorAll('.su-accordion');
 const titleButtons = document.querySelectorAll('.su-accordion__button');
+const expandButtons = document.querySelectorAll('.su-accordion__expand-all');
+const collapseButtons = document.querySelectorAll('.su-accordion__collapse-all');
 
 const isExpanded = x => x.getAttribute('aria-expanded') === 'true';
 const setExpanded = (x, value) => x.setAttribute('aria-expanded', value);
@@ -25,5 +27,27 @@ Array.prototype.forEach.call(titleButtons, btn => {
       setExpanded(btn, 'false');
       setHidden(btn.parentNode.nextElementSibling, 'true');
     }
+  }, false);
+});
+
+Array.prototype.forEach.call(expandButtons, expandBtn => {
+  expandBtn.addEventListener('click', function (e) {
+    const closestAccordion = expandBtn.closest('.su-accordion');
+    const closestBtns = closestAccordion.querySelectorAll('.su-accordion__button');
+    Array.prototype.forEach.call(closestBtns, closestBtn => {
+      setExpanded(closestBtn, 'true');
+      setHidden(closestBtn.parentNode.nextElementSibling, 'false');
+    });
+  }, false);
+});
+
+Array.prototype.forEach.call(collapseButtons, collapseBtn => {
+  collapseBtn.addEventListener('click', function (e) {
+    const closestAccordion = collapseBtn.closest('.su-accordion');
+    const closestBtns = closestAccordion.querySelectorAll('.su-accordion__button');
+    Array.prototype.forEach.call(closestBtns, closestBtn => {
+      setExpanded(closestBtn, 'false');
+      setHidden(closestBtn.parentNode.nextElementSibling, 'true');
+    });
   }, false);
 });
