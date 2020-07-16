3f62dc7fb9d5120423e0828972473b9127aed74b
Shea McKinney <sherakama@gmail.com>
2020-07-15 22:45:00 -0700

    Templates structure.

--- templates/react/components/alert/alert.js
+++ templates/react/components/alert/alert.js
@@ -1,8 +1,6 @@
 const Alert = (props) => (
   <div className="su-alert su-alert--warning">
     <div className="su-alert__body">
-      {props.deprecated ? <h4>This component has been deprecated.</h4> : null }
-      {props.experimental ? <h4>This component has experimental features.</h4> : null }
       {props.msg}
     </div>
   </div>
