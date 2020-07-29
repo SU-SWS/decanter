9e0412db6373bba7cc93dbe9fe1852ef1f4426f6
Yvonne Tang <ytang2@stanford.edu>
2020-07-28 23:10:37 -0700

    Replacing colors with new palette from updated identity toolkit

--- src/plugins/theme/fontfamily.js
+++ src/plugins/theme/fontfamily.js
@@ -4,10 +4,10 @@
 module.exports = function () {
   return {
     handwriting: ['Kalam', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
-    mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+    mono: ['Roboto Mono', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
     sans: ['Source Sans Pro', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
-    sanskrit: ['Noto Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
-    serif: ['Source Serif Pro', 'Georgia,Times', 'Times New Roman', 'serif'],
+    sanskrit: ['Noto Sans', 'sans-serif'],
+    serif: ['Source Serif Pro', 'Georgia', 'Times', 'Times New Roman', 'serif'],
     slab: ['Roboto Slab', 'Georgia', 'Times', 'Times New Roman', 'serif'],
   }
 }
