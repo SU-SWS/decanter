b46d1a2bdd734d361cfa549491f0eec56c9863e3
Yvonne Tang <ytang2@stanford.edu>
2020-07-29 00:24:37 -0700

    Use nested syntax for colors

--- src/plugins/theme/colors.js
+++ src/plugins/theme/colors.js
@@ -5,65 +5,104 @@ module.exports = function () {
   return {
     transparent: 'transparent',
     current: 'currentColor',
-    'cardinal-red': '#8C1515',
-    'cardinal-red--light': '#B83A4B',
-    'cardinal-red--dark': '#820000',
+    'cardinal-red': {
+      default: '#8C1515',
+      light: '#B83A4B',
+      dark: '#820000'
+    },
     'cool-grey': '#53565A',
-    black: '#2E2D29',
-    'black-90':'#43423E',
-    'black-80':'#585754',
-    'black-70':'#6D6C69',
-    'black-60':'#82817F',
-    'black-50':'#979694',
-    'black-40':'#ABABA9',
-    'black-30':'#C0C0BF',
-    'black-20':'#D5D5D4',
-    'black-10':'#EAEAEA',
-    'black--true': '#000000',
+    black: {
+      default: '#2E2D29',
+      '90': '#43423E',
+      '80': '#585754',
+      '70': '#6D6C69',
+      '60': '#82817F',
+      '50': '#979694',
+      '40': '#ABABA9',
+      '30': '#C0C0BF',
+      '20': '#D5D5D4',
+      '10': '#EAEAEA',
+      true: '#000000' // For when a higher contrast is needed
+    },
     white: '#FFFFFF',
-    'digital-red': '#B1040E',
-    'digital-red--light': '#EC0513',
-    'bright-blue': '#006CB8',
-    'bright-blue--light': '#6FC3FF',
-    'bright-blue--dark': '#00548f',
-    'palo-alto': '#175E54',
-    'palo-alto--light': '#2D716F',
-    'palo-alto--dark': '#014240',
-    'palo-verde': '#279989',
-    'palo-verde--light': '#59B3A9',
-    'palo-verde--dark': '#017E7C',
-    olive: '#8F993E',
-    'olive--light': '#A6B168',
-    'olive--dark': '#7A863B',
-    bay: '#6FA287',
-    'bay--light': '#8AB8A7',
-    'bay--dark': '#568474',
-    sky: '#4298B5',
-    'sky--light': '#67AFD2',
-    'sky--dark': '#016895',
-    lagunita: '#007C92',
-    'lagunita--light': '#009AB4',
-    'lagunita--dark': '#006B81',
-    poppy: '#E98300',
-    'poppy--light': '#F9A44A',
-    'poppy--dark': '#D1660F',
-    illuminating: '#FEDD5C',
-    'illuminating--light': '#FFE781',
-    'illuminating--dark': '#FEC51D',
-    plum: '#620059',
-    'plum--light': '#734675',
-    'plum--dark': '#350D36',
-    brick: '#651C32',
-    'brick--light': '#7F2D48',
-    'brick--dark': '#42081B',
-    archway: '#5D4B3C',
-    'archway--light': '#766253',
-    'archway--dark': '#2F2424',
-    stone: '#7F7776',
-    'stone--light': '#D4D1D1',
-    'stone--dark': '#544948',
-    fog: '#7F7776',
-    'fog--light': '#F4F4F4',
-    'fog--dark': '#B6B1A9',
+    'digital-red': {
+      default: '#B1040E', // Same as old Bright Red
+      light: '#EC0513' // Same as old Vivid Red
+    },
+    'bright-blue': { // Link colors
+      default: '#006CB8',
+      light: '#6FC3FF',
+      dark: '#00548f'
+    },
+    'palo-alto': {
+      default: '#175E54',
+      light: '#2D716F',
+      dark: '#014240'
+    },
+    'palo-verde': {
+      default: '#279989',
+      light: '#59B3A9',
+      dark: '#017E7C'
+    },
+    'olive': {
+      default: '#8F993E',
+      light: '#A6B168',
+      dark: '#7A863B'
+    },
+    bay: {
+      default: '#6FA287',
+      light: '#8AB8A7',
+      dark: '#568474'
+    },
+    sky: {
+      default: '#4298B5',
+      light: '#67AFD2',
+      dark: '#016895'
+    },
+    lagunita: {
+      default: '#007C92',
+      light: '#009AB4',
+      dark: '#006B81'
+    },
+    poppy: {
+      default: '#E98300',
+      light: '#F9A44A',
+      dark: '#D1660F'
+    },
+    spirited: {
+      default: '#E04F39',
+      light: '#F4795B',
+      dark: '#C74632'
+    },
+    illuminating: {
+      default: '#FEDD5C',
+      light: '#FFE781',
+      dark: '#FEC51D'
+    },
+    plum: {
+      default: '#620059',
+      light: '#734675',
+      dark: '#350D36'
+    },
+    brick: {
+      default: '#651C32',
+      light: '#7F2D48',
+      dark: '#42081B'
+    },
+    archway: {
+      default: '#5D4B3C',
+      light: '#766253',
+      dark: '#2F2424' // Same as old Chocolate
+    },
+    stone: {
+      default: '#7F7776',
+      light: '#D4D1D1',
+      dark: '#544948' // Same as old Stone
+    },
+    fog: {
+      default: '#7F7776',
+      light: '#F4F4F4', // Same as old Fog
+      dark: '#B6B1A9' // Same as old Driftwood
+    }
   }
 }
