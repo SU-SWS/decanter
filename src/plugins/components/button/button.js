26483bd4961f4a8bc259cfa63c17be0de88db05c
Yvonne Tang <ytang2@stanford.edu>
2020-08-12 02:35:04 -0700

    Add button style as plugin component instead; font family array items with multiple words need double quotes inside single quotes

--- src/plugins/components/button/button.js
+++ src/plugins/components/button/button.js
@@ -1,8 +1,19 @@
 module.exports = function () {
-  return function ({ addComponents, config }) {
+  return function ({ addComponents, theme, config }) {
+    const sans = config('theme.fontFamily.sans').join(', ');
+
     const button = {
       '.button': {
         padding: '1rem 2rem 1.15rem',
+        display: 'inline-block',
+        width: 'auto',
+        border: 'none',
+        backgroundColor: theme('colors.digital-red.default'),
+        color: theme('colors.white'),
+        textAlign: 'center',
+        fontFamily: sans,
+        fontWeight: '400',
+        transition: 'background-color 0.25s ease-in-out, color 0.25s ease-in-out',
       },
     }
 
