85353681bb72d744252d6866987ca8a926b0e00c
Yvonne Tang <ytang2@stanford.edu>
2020-08-13 17:15:37 -0700

    Lockup styles, with option D styles

--- src/plugins/components/lockup/lockup.js
+++ src/plugins/components/lockup/lockup.js
@@ -16,7 +16,7 @@ module.exports = function () {
             alignItems: 'stretch',
             JustifyContent: 'flex-start',
             verticalAlign: 'bottom',
-          }
+          },
         },
         'a': {
           fontWeight: theme('fontWeight.regular'),
@@ -26,7 +26,7 @@ module.exports = function () {
           width: 'auto',
           fontSize: '36px',
           minHeight: '26px',
-          verticalAlign: 'botton',
+          verticalAlign: 'bottom',
           '@screen md': {
             display: 'flex',
             flexShrink: '1',
@@ -37,7 +37,7 @@ module.exports = function () {
             maxWidth: '160px',
             minHeight: '32px',
             borderRight: '1px solid ' + theme('colors.black.default'),
-          }
+          },
         },
         '&__cell2': {
           '@screen md': {
@@ -47,7 +47,7 @@ module.exports = function () {
             borderLeft: '1px solid ' + theme('colors.black.default'),
             flexGrow: '1',
             verticalAlign: 'bottom',
-          }
+          },
         },
         '.logo': {
           color: theme('colors.cardinal-red.default'),
@@ -58,19 +58,75 @@ module.exports = function () {
           '@screen md': {
             fontSize: '46px',
             lineHeight: '21px',
-          }
+          },
         },
         '&__wordmark-wrapper': {
           lineHeight: '26px',
 
           '@screen md': {
             lineHeight: '32px',
-          }
+          },
         },
         '&__line1, &__line2, &__line3, &__line4, &__line5': {
           display: 'block',
           color: theme('colors.black.default'),
-        }
+        },
+        '&__line1': {
+          fontSize: '2.6rem',
+          '@screen md': {
+            fontSize: '2.8rem',
+          },
+        },
+        '&__line2': {
+          fontSize: '2.6rem',
+        },
+        '&__line3': {
+          margin: '0',
+          fontStyle: 'italic',
+          fontSize: '1.9rem',
+          '@screen md': {
+            marginTop: '0.5rem',
+            fontSize: '2.6rem',
+          },
+        },
+        '&__line4': {
+          marginTop: '0.5rem',
+          marginLeft: '-0.2rem',
+          fontSize: '2.5rem',
+          fontWeight: theme('fontWeight.semibold'),
+          letterSpacing: '0.05rem',
+          lineHeight: theme('lineHeight.none'),
+          textTransform: 'uppercase',
+        },
+        '&__line5': {
+          fontSize: '2.7rem',
+          lineHeight: theme('lineHeight.none'),
+          width: '100%',
+          '@screen md': {
+            marginTop: '8px',
+            fontSize: '3rem',
+          },
+        },
+      },
+      // Lockup option D: Unit + Level (1 line)
+      '.lockup--option-d': {
+        '.lockup__line2, .lockup__line4, .lockup__line5': {
+          display: 'none',
+        },
+        '.lockup__line1': {
+          marginTop: '0.5rem',
+          marginBottom: '0.2rem',
+          marginLeft: '-0.2rem',
+          '@screen md': {
+            marginTop: '-0.7rem',
+          },
+        },
+        '.lockup__line3': {
+          '@screen md': {
+            fontSize: '1.8rem',
+            lineHeight: '0.7em',
+          },
+        },
       },
     }
 
