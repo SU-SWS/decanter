a3d918a1df8385cce39fbbbc20b252553d655d73
Shea McKinney <sherakama@gmail.com>
2020-08-31 22:17:17 -0700

    fixup! styles and javascript close on alerts.

--- core/src/js/components/components.js
+++ core/src/js/components/components.js
@@ -1,6 +1,8 @@
 /**
  * Primary roll up file for all javascript components.
  */
+ // The Alert Component.
+ import './alert/alert.js';
 // The Accordion Component.
 import './accordion/accordion.js';
 // The Primary Navigation Component.
