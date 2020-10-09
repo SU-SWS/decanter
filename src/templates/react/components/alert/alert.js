ef485ebd74982946c8450c966172b1f8c6e5c4cd
Sherakama <sherakama@gmail.com>
2020-10-08 23:18:19 -0700

    Remove Template Files. (#765)

--- src/templates/react/components/alert/alert.js
+++
@@ -1,9 +0,0 @@
-const Alert = (props) => (
-  <div className="su-alert su-alert--warning">
-    <div className="su-alert__body">
-      {props.msg}
-    </div>
-  </div>
-);
-
-export default Alert;
--- src/templates/react/components/button/button.js
+++
@@ -1,7 +0,0 @@
-const Button = (props) => (
-  <button className="su-button su-bg-digital-red su-text-white hover:su-bg-black focus:su-bg-black su-font-regular su-leading-none">
-    {props.label}
-  </button>
-);
-
-export default Button;
