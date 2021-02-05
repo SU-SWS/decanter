4a4e72c1d840ecf136c5a134a8e42374b402f867
Yvonne Tang <ytang2@stanford.edu>
2021-02-04 18:19:33 -0800

    Height, width and maxheight by TW default produces classes using all of the spacing values - turn it off

---
+++ src/plugins/theme/maxHeight.js
@@ -0,0 +1,9 @@
+/**
+ * Max Heights - customized to not produce classes with our custom spacing values
+ */
+module.exports = function () {
+  return {
+    full: '100%',
+    screen: '100vh',
+  }
+}
