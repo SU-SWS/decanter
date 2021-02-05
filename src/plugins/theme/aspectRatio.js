fe238e7f9e8aac2539abbfc839e3cefaea3c64bd
Yvonne Tang <ytang2@stanford.edu>
2021-02-04 18:55:04 -0800

    Set up aspect ratio classes properly using the new plugin configs

--- src/plugins/theme/aspectRatio.js
+++ src/plugins/theme/aspectRatio.js
@@ -3,10 +3,11 @@
  */
 module.exports = function () {
   return {
-    'none': 0,
-    'square': [1, 1],
-    DEFAULT: [16, 9], // Popular photo aspect ratio
-    '4x3': [4, 3],
-    '2x1': [2, 1], // Our usual crop for cards
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    9: '9',
+    16: '16',
   }
 }
