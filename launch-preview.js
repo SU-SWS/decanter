deb3f4e28ca4ed325aca729ae53fe6989d0c4eb6
Shea McKinney <sherakama@gmail.com>
2021-02-08 14:26:39 -0800

    fixup! stuff.

--- launch-preview.js
+++ launch-preview.js
@@ -7,6 +7,7 @@ exec('git branch --show-current', function (error, stdout, stderr) {
   branch = stdout;
 });
 exec('cd styleguide');
+exec('rm package-lock.json');
 exec('nvm use && nvm install');
 exec('npm install "https://github.com/su-sws/decanter.git#' + branch + '" --save --force');
 exec('npm run export');
