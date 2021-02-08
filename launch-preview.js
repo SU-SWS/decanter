3513bec81427402fe9a1a166ca61c69797842d7f
Shea McKinney <sherakama@gmail.com>
2021-02-08 14:11:05 -0800

    Switch to Netlify For Build Previews.

---
+++ launch-preview.js
@@ -0,0 +1,12 @@
+const { exec } = require('child_process');
+let branch = 'master';
+
+exec('rm -Rf ./styleguide');
+exec('git clone https://github.com/su-sws/decanter-web.git styleguide');
+exec('git branch --show-current', function(error, stdout, stderr) {
+  branch = stdout;
+});
+exec('cd styleguide');
+exec('nvm use && nvm install');
+exec('npm install "https://github.com/su-sws/decanter.git#' + branch + '" --save --force');
+exec('npm run export');
