7fef2ca9dc8fd2b0e745b9dec9f85f650509aa5a
Shea McKinney <sherakama@gmail.com>
2021-02-08 16:05:19 -0800

    fixup! more fixes.

--- launch-preview.js
+++ launch-preview.js
@@ -1,13 +1,23 @@
-const {exec} = require('child_process');
+const {execSync} = require('child_process');
 let branch = 'master';
 
-exec('rm -Rf ./styleguide');
-exec('git clone https://github.com/su-sws/decanter-web.git styleguide');
-exec('git branch --show-current', function (error, stdout, stderr) {
-  branch = stdout;
-});
-exec('cd styleguide');
-exec('rm package-lock.json');
-exec('nvm use && nvm install');
-exec('npm install "https://github.com/su-sws/decanter.git#' + branch + '" --save --force');
-exec('npm run export');
+// Get the Current Branch.
+branch = execSync('git branch --show-current', {encoding: 'utf8'});
+
+// Try deleting the styleguide if it exists. (cache)
+execSync('rm -Rf ./styleguide');
+
+// Clone fresh.
+execSync('git clone https://github.com/su-sws/decanter-web.git styleguide');
+
+// Add this branch as a dependency.
+execSync('cd styleguide && npm install "https://github.com/su-sws/decanter.git#' + branch + '" --save --force');
+
+// Install.
+execSync('cd styleguide && npm install');
+
+// Build the styleguide.
+execSync('cd styleguide && npm run export');
+
+// Boom shakalaka.
+console.log('All Done');
