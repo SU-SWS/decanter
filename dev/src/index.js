a2582a01e8fb059fda72e8f073a01da52e0b5f3b
Shea McKinney <sherakama@gmail.com>
2021-02-17 17:18:28 -0800

    Fixup webpack html builder.

--- dev/src/index.js
+++ dev/src/index.js
@@ -4,5 +4,8 @@ import './test.css';
 // Local javascript.
 import './test.js';
 
+// HTML
+import './test.html';
+
 // Say hi.
 console.log("Welcome to Decanter Preview Environment. Everything seems to be working.");
