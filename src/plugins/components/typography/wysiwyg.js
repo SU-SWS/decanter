fb2619397918ffbfd16465c750ad8de38bb6bf5b
Yvonne Tang <ytang2@stanford.edu>
2021-03-12 17:00:10 -0800

    Unstructured WYSIWYG content as TW component; tweak button base styles

---
+++ src/plugins/components/typography/wysiwyg.js
@@ -0,0 +1,51 @@
+/**
+ * Sets the vertical rhythm, i.e., spacing between elements such as heading, paragraphs and lists
+ * within unstructured WYSIWYG content.
+ */
+module.exports = function () {
+  return function ({ addComponents, theme }) {
+
+    const components = {
+      '.wysiwyg': {
+        '> *:first-child': {
+          marginTop: '-0.25em', // Leading trim
+        },
+        '> *:last-child': {
+          marginBottom: '0',
+        },
+        'p, ol, ul, dl': {
+          color: theme('color.black.true'),
+          paddingBottom: '1.6em',
+        },
+        'li p': {
+          marginBottom: '0.4em',
+        },
+        'h2:not(:first-child)': {
+          marginTop: '1.8em',
+        },
+        'h3, h4, h5, h6': {
+          '&:not(:first-child)': {
+            marginTop: '1.6em',
+          }
+        },
+        'h2, h3, h4, h5, h6': {
+          marginBottom: '0.7em',
+          clear: 'both',
+          'a': {
+            textDecoration: 'none',
+          },
+          'a:hover, a:focus': {
+            textDecoration: 'underline',
+          }
+        },
+        'h2 + h3, h3 + h4, h4 + h5, h5 + h6': {
+          '&:not(:first-child)': {
+            marginTop: '1em',
+          }
+        },
+      },
+    }
+
+    addComponents(components)
+  }
+}
