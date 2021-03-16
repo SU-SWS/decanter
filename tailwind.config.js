c161d40f5ca0d82871ca2a280c18560db065ac3f
Yvonne Tang <ytang2@stanford.edu>
2021-03-16 13:18:43 -0700

    Remove animation variant but add translate variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -97,8 +97,8 @@ module.exports = {
   // core utility plugin.
   // https://tailwindcss.com/docs/configuration/#variants
   variants: {
-    animation: ['hover', 'focus', 'hocus', 'group-hover', 'group-focus', 'group-hocus', 'motion-safe', 'motion-reduce'],
     extend: {
+      translate: ['hocus', 'group-hocus', 'motion-safe', 'motion-reduce'],
       margin: ['first', 'last'],
       backgroundColor: ['group-focus-within', 'group-focus-visible', 'group-active', 'hocus', 'group-hocus'],
       borderColor: ['group-focus-within', 'hocus', 'group-hocus'],
