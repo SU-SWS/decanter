a41e7e4a5bfd324c9658b50512eb828167f4344d
Yvonne Tang <ytang2@stanford.edu>
2023-05-03 16:00:20 -0700

    Update TW to 3.3.2; uninstall TW line-clamp plugin; add gap options

--- src/plugins/theme/gap.js
+++ src/plugins/theme/gap.js
@@ -3,9 +3,24 @@
  */
 module.exports = function () {
   return {
+    // The following are tied to our breakpoints in src/plugins/theme/screens.js
     xs: '20px',
     lg: '36px',
     xl: '40px',
     '2xl': '48px',
+    // Additional commonly used gutters for convenience
+    1: '1px',
+    5: '5px',
+    10: '10px',
+    15: '15px',
+    20: '20px', // Alias for gap-xs
+    30: '30px',
+    40: '40px', // Alias for gap-xl
+    50: '50px',
+    60: '60px',
+    70: '70px',
+    80: '80px',
+    90: '90px',
+    100: '100px',
   };
 };
