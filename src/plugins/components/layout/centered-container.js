b09d4888aaaa636d5cf4572d727766ff4d10d33c
pookmish <pookmish@stanford.edu>
2023-05-19 17:23:26 -0700

    Change centered-container to use max width and allow nesting of classes

--- src/plugins/components/layout/centered-container.js
+++ src/plugins/components/layout/centered-container.js
@@ -2,36 +2,38 @@
  * Centered container.
  */
 module.exports = function () {
-  return function ({ addComponents, theme }) {
+  return function ({ addUtilities, theme }) {
     // Find and set the padding based on the screen margins
     const screens = theme('screens');
-    const padding = {};
+    const maxWdiths = {};
 
-    // Create padding for each screen size which equals to the screen margin setting.
+    // Create max widths for each screen size which equals to the screen margin
+    // setting.
     const keys = Object.keys(screens);
     keys.forEach((key) => {
-      padding[`@screen ${key}`] = {
-        paddingLeft: theme(`decanter.screenMargins.${key}`),
-        paddingRight: theme(`decanter.screenMargins.${key}`),
-      };
+      if (theme(`decanter.screenMargins.${key}`)) {
+        const gutterWidth = parseInt(theme(`decanter.screenMargins.${key}`));
+
+        maxWdiths[`@screen ${key}`] = {
+          maxWidth: 'calc(100vw - ' + (gutterWidth * 2) + 'px)',
+        };
+      }
     });
 
     const components = {
       // Center an element horizontally.
       '.centered-container, .cc': {
-        paddingLeft: theme('decanter.screenMargins.xs'),
-        paddingRight: theme('decanter.screenMargins.xs'),
+        maxWidth: 'calc(100vw - ' + (parseInt(theme(`decanter.screenMargins.xs`)) * 2) + 'px)',
+        width: '100%',
         marginLeft: 'auto',
         marginRight: 'auto',
-        ...padding,
-        // At 1700px (2xl breakpoint + twice the screen margins at 2xl), the max container width stays at 1500px.
+        ...maxWdiths,
         '@media only screen and (min-width: 1700px)': {
-          paddingLeft: 'calc((100% - 1500px)/2)',
-          paddingRight: 'calc((100% - 1500px)/2)',
+          maxWidth: '1500px'
         },
       },
     };
 
-    addComponents(components);
+    addUtilities(components);
   };
 };
