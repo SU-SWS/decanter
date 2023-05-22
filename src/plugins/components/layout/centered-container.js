69622c357f28f48653e3e65046bc8fa0ddc1ca4c
pookmish <pookmish@stanford.edu>
2023-05-21 18:22:44 -0700

    Make the centered-container class more dynamic

--- src/plugins/components/layout/centered-container.js
+++ src/plugins/components/layout/centered-container.js
@@ -6,30 +6,38 @@ module.exports = function () {
     // Find and set the padding based on the screen margins
     const screens = theme('screens');
     const maxWdiths = {};
+    let largestScreen = 0;
+    let largestGutter = 0;
+    let smallestGutter = 999999;
 
     // Create max widths for each screen size which equals to the screen margin
     // setting.
     const keys = Object.keys(screens);
     keys.forEach((key) => {
+      if (largestScreen < parseInt(screens[key])) largestScreen = parseInt(screens[key]);
+
       if (theme(`decanter.screenMargins.${key}`)) {
         const gutterWidth = parseInt(theme(`decanter.screenMargins.${key}`));
+        if (largestGutter < gutterWidth) largestGutter = gutterWidth;
+        if (smallestGutter > gutterWidth) smallestGutter = gutterWidth;
 
         maxWdiths[`@screen ${key}`] = {
           maxWidth: 'calc(100vw - ' + (gutterWidth * 2) + 'px)',
         };
       }
     });
-
+      
+    const key = `@media only screen and (min-width: ${largestScreen + (largestGutter * 2)}px)`
     const components = {
       // Center an element horizontally.
-      '.centered-container, .cc': {
-        maxWidth: 'calc(100vw - ' + (parseInt(theme(`decanter.screenMargins.xs`)) * 2) + 'px)',
+      '.centered': {
+        maxWidth: 'calc(100vw - ' + (smallestGutter * 2) + 'px)',
         width: '100%',
         marginLeft: 'auto',
         marginRight: 'auto',
         ...maxWdiths,
-        '@media only screen and (min-width: 1700px)': {
-          maxWidth: '1500px'
+        [key]: {
+          maxWidth: `${largestScreen}px`,
         },
       },
     };
