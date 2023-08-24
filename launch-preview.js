1ea7d6996645c62867e6fb7b3d55ca246db45c34
Shea McKinney <sheamck@stanford.edu>
2023-08-24 10:52:32 -0700

    For demo.

--- launch-preview.js
+++ launch-preview.js
@@ -5,21 +5,27 @@ let branch = 'master';
 branch = execSync('git branch --show-current', {encoding: 'utf8'});
 
 // Try deleting the styleguide if it exists. (cache)
+console.log('Starting...');
 execSync('rm -Rf ./styleguide');
 
 // Clone fresh.
+console.log('Cloning...');
 execSync('git clone https://github.com/su-sws/decanter-web.git styleguide');
 
 // Change to the testing branch.
+console.log('Checking out...');
 execSync('cd styleguide && git checkout task/package-updates');
 
 // Add this branch as a dependency.
-execSync('cd styleguide && npm install github:SU-SWS/decanter#' + branch + ' --save --force');
+console.log('Adding dependency...');
+execSync('cd styleguide && npm install github:SU-SWS/decanter#' + branch);
 
 // Install.
+console.log('Installing...');
 execSync('cd styleguide && npm install');
 
 // Build the styleguide.
+console.log('Building...');
 execSync('cd styleguide && npm run export');
 
 // Boom shakalaka.
